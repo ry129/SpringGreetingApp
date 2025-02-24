b2aabb7c2748974bd4715efabf5b6cce606b9484
ry129 <0111cs211146.riya@gmail.com>
2025-02-24 11:48:53 +0530

    [Riya] Add. Added Ability to Greeting App

--- src/main/java/com/greetingapp/Service/GreetingService.java
+++ src/main/java/com/greetingapp/Service/GreetingService.java
@@ -5,9 +5,17 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class GreetingService {
-    public String getgreeting() {
-        return "Hello From Greeting Service";
+    public String getgreetingMessage(String firstName, String lastName) {
+        if(firstName!=null && lastName!=null) {
+            return  "Hello" + firstName + " " + lastName;
+        }
+        else if(firstName!=null) {
+            return  "Hello " + firstName;
+        }
+        else if(lastName!=null) {
+            return  "Hello " + lastName;
+        }
+        return "Hello World";
+
     }
-
-
 }
