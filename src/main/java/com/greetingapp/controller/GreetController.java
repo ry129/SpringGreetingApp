2e48089829efc0a32b58ce632bd6c9d3d1c16999
ry129 <0111cs211146.riya@gmail.com>
2025-02-24 10:57:00 +0530

    [Riya] Add. GreetingController that return JSON for different HTTP Methods.

---
+++ src/main/java/com/greetingapp/controller/GreetController.java
@@ -0,0 +1,29 @@
+package com.greetingapp.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/greeting")
+public class GreetController {
+    @GetMapping
+    public Map<String, String> getGreeting() {
+        return  Map.of("message", "Hello GET Request!");
+    }
+
+    @PostMapping
+    public Map<String, String> postGreeting(@RequestBody Map<String, String> body) {
+        return Map.of("message", "Hello POST Request! ", "data", body.get("name"));
+    }
+
+    @PutMapping
+    public Map<String, String> putGreeting(@RequestBody Map<String, String> body) {
+        return Map.of("message", "Hello PUT Request!", "data", body.get("name"));
+    }
+
+    @DeleteMapping
+    public Map<String, String> deleteGreeting() {
+        return Map.of("message", "Hello, DELETE Request!");
+    }
+}
